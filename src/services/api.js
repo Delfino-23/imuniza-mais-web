1e57e5e75fd68748cbc752c06ef7b4e5e320684e
Delfino-23 <delfino.carlosedu@gmail.com>
2026-07-17 19:02:32 -0300

    feat: corrigir url da api

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
 
-    baseURL: 'http://localhost:4000/api',
+    // baseURL: 'http://localhost:4000/api',
+    baseURL: 'https://imuniza-mais-api.onrender.com/api',
 
     headers: {
         'Content-Type': 'application/json'
